e79fe331ebb29815bae7ad8a6762872f52c4ca43
nikita-agarwal <nagarwal201@gmail.com>
2022-01-08 22:02:33 +0530

    Update customer.service.ts

--- mudy/src/app/customer.service.ts
+++ mudy/src/app/customer.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import 'rxjs/add/operator/map';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -11,25 +12,25 @@ export class CustomerService {
   };
   constructor(private http: HttpClient) { }
   getCustomers() {
-    return this.http.get<any>('http://localhost:3000/api/customers')
+    return this.http.get<any>(environment.apiUrl+'/api/customers')
       .map(res => res);
   }
   getAllStates() {
-    return this.http.get<any>('http://localhost:3000/api/states')
+    return this.http.get<any>(environment.apiUrl+'/api/states')
       .map(res => res);
   }
   addCustomer(newCustomer) {
     const headers1 = new HttpHeaders();
     headers1.append('Content-Type', 'application/javascript');
     headers1.append('Access-Control-Allow-Origin', '*');
-    return this.http.post<any>('http://localhost:3000/api/customer', newCustomer, { headers: headers1 })
+    return this.http.post<any>(environment.apiUrl+'/api/customer', newCustomer, { headers: headers1 })
       .map(res => res);
   }
   updateCustomer(id, sub) {
     const headers1 = new HttpHeaders();
     headers1.append('Content-Type', 'application/javascript');
     headers1.append('Access-Control-Allow-Origin', '*');
-    return this.http.put<any>('http://localhost:3000/api/updateCustomer/' + id, { subRates: sub }, { headers: headers1 })
+    return this.http.put<any>(environment.apiUrl+'/api/updateCustomer/' + id, { subRates: sub }, { headers: headers1 })
       .map(res => res);
   }
 
@@ -37,23 +38,23 @@ export class CustomerService {
     const headers1 = new HttpHeaders();
     headers1.append('Content-Type', 'application/javascript');
     headers1.append('Access-Control-Allow-Origin', '*');
-    return this.http.put<any>('http://localhost:3000/api/updateMaxRateChanges/' + id, { number: num }, { headers: headers1 })
+    return this.http.put<any>(environment.apiUrl+'/api/updateMaxRateChanges/' + id, { number: num }, { headers: headers1 })
       .map(res => res);
   }
   deleteCustomer(id) {
-    return this.http.delete<any>('http://localhost:3000/api/customer/' + id)
+    return this.http.delete<any>(environment.apiUrl+'/api/customer/' + id)
       .map(res => res);
   }
   getCustomerById(id) {
-    return this.http.get<any>('http://localhost:3000/api/customer/' + id)
+    return this.http.get<any>(environment.apiUrl+'/api/customer/' + id)
       .map(res => res);
   }
   getInvoices() {
-    return this.http.get<any>('http://localhost:3000/api/invoices')
+    return this.http.get<any>(environment.apiUrl+'/api/invoices')
       .map(res => res);
   }
   getInvoiceById(id) {
-    return this.http.get<any>('http://localhost:3000/api/invoice/' + id)
+    return this.http.get<any>(environment.apiUrl+'/api/invoice/' + id)
       .map(res => res);
   }
   addInvoice(name, customerId, items, invoiceDate) {
@@ -61,13 +62,13 @@ export class CustomerService {
     headers1.append('Content-Type', 'application/javascript');
     headers1.append('Access-Control-Allow-Origin', '*');
     const filename = name + '-' + invoiceDate;
-    return this.http.post<any>('http://localhost:3000/api/invoice',
+    return this.http.post<any>(environment.apiUrl+'/api/invoice',
       { name: filename, id: customerId, goods: items, date: invoiceDate }, { headers: headers1 })
       .map(res => res);
   }
 
   deleteInvoice(id) {
-    return this.http.delete<any>('http://localhost:3000/api/invoice/' + id)
+    return this.http.delete<any>(environment.apiUrl+'/api/invoice/' + id)
       .map(res => res);
   }
 }
